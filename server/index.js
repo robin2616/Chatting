a2ea2eb6751728717d1bdd7f229b4568f33c4170
robin2616 <robinkumardbg11@gmail.com>
2024-06-03 16:24:27 +0530

    sf

--- server/index.js
+++ server/index.js
@@ -1,20 +1,36 @@
-const express=require('express');
-const app = express();
-const mongoose=require('mongoose');
-const port=10000;
-const cors=require('cors');
- const bodyParser=require('body-parser');
-console.log("hello world");
- app.use(cors());
- app.use(bodyParser.json());
+const mongoose= require(id:"mongoose");
+const uri="mongodb+srv://robinkumardbg11:aPTJ!E8LbWEgZZf@first.qxoadwq.mongodb.net/Chatting"
+mongoose.connect(uri);
 
-main().catch(err=>console.log(err))
-async function main(){
-    await mongoose.connect('mongodb+srv://robinkumardbg11:aPTJ!E8LbWEgZZf@first.qxoadwq.mongodb.net/')
-    console.log("db connected")
-}
 
-app.post('/passfetch',async(req,res)=>{
-    const user=await Product.find({password:req.body.password})
-    res.json(user)
-    })
+
+
+
+
+
+
+// const express=require('express');
+// const app = express();
+// const mongoose=require('mongoose');
+// const port=3000;
+// const cors=require('cors');
+//  const bodyParser=require('body-parser');
+// console.log("hello world");
+//  app.use(cors());
+//  app.use(bodyParser.json());
+
+// main().catch(err=>console.log(err))
+// async function main(){
+//     await mongoose.connect('mongodb+srv://robinkumardbg11:aPTJ!E8LbWEgZZf@first.qxoadwq.mongodb.net/')
+//     console.log("db connected")
+// }
+
+// app.post('/passfetch',async(req,res)=>{
+//     const user=await Product.find({password:req.body.password})
+//     res.json(user)
+//     console.log(user)
+//     })
+
+//     app.listen(port,()=>{
+//         console.log(`listening on port ${port}`)
+//     });=
