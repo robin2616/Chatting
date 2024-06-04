5da2b783a18d04e250b44263beb7a4930409c5ce
robin2616 <robinkumardbg11@gmail.com>
2024-06-04 12:46:23 +0530

    df

--- chat/src/Home.jsx
+++ chat/src/Home.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { useContext, useState } from 'react'
 import { Mycontext } from './Context'
+import { FaHeart } from "react-icons/fa";
 
 
 function Home() {
     const { addname } = useContext(Mycontext)
     const [a, b] = useState()
     return (<>
-        <div  class="form-floating mb-3 w-[200px] absolute top-[230px] left-[40%] border-blue-500 border-2 rounded-xl">
-            <input  type="email" class="form-control" value={a} onChange={(e) => { b(e.target.value) }} id="floatingInput" placeholder="name@example.com" />
-            <label for="floatingInput">Guchi Puchi</label>
+        <div  class="form-floating mb-3 w-[200px] absolute top-[230px] left-[38%] border-blue-500 border-2 rounded-xl">
+            <input  type="string" class="form-control" value={a} onChange={(e) => { b(e.target.value) }} id="floatingInput" placeholder="name@example.com" />
+            <label for="floatingInput">Guchi Puchi ki Mummy</label>
         </div>
 
-        <button type="button" class="btn btn-success absolute top-[300px] left-[44%] w-[100px]" onClick={async () => {
+        <button type="button" class="btn btn-success absolute top-[300px] left-[42%] w-[100px]" onClick={async () => {
 
 
             const items = { password: a }
@@ -37,6 +38,16 @@ function Home() {
 
 
         }}>Login</button>
+        <span><FaHeart className='text-red-500 text-[100px] absolute top-[220px] left-[360px]'/></span>
+        <span><FaHeart className='text-red-500 text-[100px] absolute top-[220px] left-[720px]'/></span>
+        <span><FaHeart className='text-red-500 text-[100px] absolute top-[110px] left-[360px]'/></span>
+        <span><FaHeart className='text-red-500 text-[100px] absolute top-[110px] left-[480px]'/></span>
+        <span><FaHeart className='text-red-500 text-[100px] absolute top-[110px] left-[600px]'/></span>
+        <span><FaHeart className='text-red-500 text-[100px] absolute top-[110px] left-[720px]'/></span>
+        <span><FaHeart className='text-red-500 text-[100px] absolute top-[350px] left-[360px]'/></span>
+        <span><FaHeart className='text-red-500 text-[100px] absolute top-[350px] left-[480px]'/></span>
+        <span><FaHeart className='text-red-500 text-[100px] absolute top-[350px] left-[600px]'/></span>
+        <span><FaHeart className='text-red-500 text-[100px] absolute top-[350px] left-[720px]'/></span>
     </>)
 }
 export default Home
--- chat/src/Mess.jsx
+++ chat/src/Mess.jsx
@@ -3,6 +3,12 @@ import { useContext } from "react"
 import { Mycontext } from "./Context"
 import { useState } from "react"
 import { useEffect } from "react"
+import './mess.css'
+import { useRef } from "react"
+import { FiRefreshCcw } from "react-icons/fi";
+import { IoSend } from "react-icons/io5";
+
+
 
 function Mess() {
     const { name } = useContext(Mycontext)
@@ -12,6 +18,13 @@ function Mess() {
      const localTime = now.toLocaleTimeString();
      const localdate=now.toLocaleDateString();
 const[x,y]=useState([])
+
+const handleKeyPress = (event) => {
+    // Check if Enter key is pressed
+    if (event.key === 'Enter') {
+      document.getElementById('myButton').click();
+    }
+  };
 
 useEffect(()=>{
 const showmess=async()=>{
@@ -29,32 +42,49 @@ showmess();
 
 },[])
 
+const chatBoxRef = useRef(null);
+
+useEffect(() => {
+    // Scroll to the bottom of the chat box when the component mounts or messages update
+    if (chatBoxRef.current) {
+      chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+    }
+  }, [x]);
+
 console.log(x)
 
     return (<>
-<div className=" w-[600px] h-[400px] overflow-scroll overflow-x-hidden border-black border-2 absolute top-[60px] left-[370px] bg-blue-100">
+    
+    <span className="text-4xl sm:text-6xl font-bold absolute left-[38%] text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">Guchi Puchi</span>
+      <div ref={chatBoxRef}   class="w-[600px] h-[400px] absolute top-[60px] left-[370px] bg-white rounded-lg shadow-xl overflow-y-auto custom-scrollbar p-4">
+    <div  class="space-y-4">
+       {/* <div className=" bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">asjndfjkanfs</div> */}
 {x.map((i)=>(<>
-     <div className=" flex flex-row gap-4 border-black border-2" >
-        <span className="ml-10 mt-2">{i.bywhom}</span>
-        <div className=" mt-2 w-[370px] writing-mode-vertical-rl text-orientation-mixed break-words h-[auto]">{i.message}</div>
-       <div className=" flex flex-col">
-        <span className=" scale-90">{i.time}</span>
-        <span className="">{i.date}</span>
-        </div>
-    </div> 
-    </>  
+    <div class="bg-blue-100 flex h-[auto] flex-row p-3 gap-5 rounded-lg shadow-md">
+            <p class=" text-blue-900 text-xl font-semibold translate-y-1">{i.bywhom}</p>
+            <p className=" text-blue-900 w-[300px] writing-mode-vertical-rl text-orientation-mixed break-words h-[auto] text-lg font-semibold translate-y-1">{i.message}</p>
+            <div className=" flex flex-col gap-0 translate-y-[-10px] scale-[0.7]">
+            <p class=" text-blue-900 text-sm font-semibold ">{i.time}</p>
+            <p class=" text-blue-900 text-sm font-semibold translate-y-[-10px]">{i.date}</p>
+            </div>
+    </div>     
+        </>
 ))}
 
-</div>
+     
+      </div>
+      </div>
 
-        <div class="form-floating mb-3 absolute top-[470px] w-[500px] left-[370px]">
-            <input type="text" value={c} onChange={(e)=>{d(e.target.value)}} class="form-control" id="floatingInput"  />
+
+
+        <div class="form-floating shadow-lg mb-3 absolute top-[470px] w-[500px] left-[370px]">
+            <input onKeyDown={handleKeyPress} type="text" value={c} onChange={(e)=>{d(e.target.value)}} class="form-control"   />
             <label for="floatingInput">Write Message</label>
         </div>
         
 
 
-        <button type="button" 
+        <button type="button" id="myButton"
         onClick={async()=>{
             const share={name:name,message:c,time:localTime,date:localdate}
 
@@ -74,8 +104,9 @@ console.log(x)
 
 
         }}
-        class="btn btn-info absolute top-[475px] w-[100px] h-[50px] left-[880px]">Send</button>
+        class="btn shadow-lg btn-info absolute top-[475px] w-[100px] h-[50px] left-[880px]"><IoSend className=" text-2xl translate-x-6"/></button>
 
+<button className="text-2xl absolute top-[470px] left-[1000px] bg-yellow-300 p-3 rounded-xl" onClick={()=>{location.reload()}}><FiRefreshCcw/></button>
     </>)
 }
 export default Mess
